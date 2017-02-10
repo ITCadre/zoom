48cd950913d1538f9c208cc4d2edcf0bdbe33ca4
Waipang Fong <waipang.fong@itcadre.com>
2017-02-10 10:08:57 -0500

    tt

--- zoom/views.py
+++ zoom/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-#from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 
 
 
@@ -12,11 +12,13 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 @permission_classes((IsAuthenticated,))
 def login(request, format=None):
 
-   # token = Token.objects.create(user = request.user)
+    token = Token.objects.get(user = request.user)
+
+    print (token.key)
     content = {
         'user': str(request.user),  # `django.contrib.auth.User` instance.
         'auth': str(request.auth),  # None
-    #    'token': token.key,
+        'token': token.key,
 
 
     }
